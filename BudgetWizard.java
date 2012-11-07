78c2d76b9fa7c3843c55f90228ccea5bdd617a2b
Nathan Stien <nathanism@gmail.com>
2012-11-06 21:11:35 -0600

    started working on Build Your Budget

--- BudgetWizard.java
+++ BudgetWizard.java
@@ -3,3 +3,5 @@ Let's preteend this is the code for BudgetWizard.
 This line has a bug we don't know about yet.
 
 Here's some more perfectly fine code.
+
+This is the half-finished Build Your Budget code.
