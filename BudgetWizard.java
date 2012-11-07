cd803744b439cec2e4223a22cefb6251a76b54fe
Nathan Stien <nathanism@gmail.com>
2012-11-06 21:30:55 -0600

    finally finished BYB!

--- BudgetWizard.java
+++ BudgetWizard.java
@@ -6,6 +6,4 @@ This line addresses incident 124 somehow.
 
 Here's some more perfectly fine code.
 
-This is the half-finished Build Your Budget code.
-Here's some more progress on that.
-We're getting closer on BYB now.
+Build Your Budget is finished!
