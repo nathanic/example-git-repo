51d92b7f3e75c2b2d476e40ef09d13bff2086db1
Nathan Stien <nathanism@gmail.com>
2012-11-06 21:14:46 -0600

    fixed incident 124 easily

--- BudgetWizard.java
+++ BudgetWizard.java
@@ -2,4 +2,6 @@ Let's preteend this is the code for BudgetWizard.
 
 This line has a bug we don't know about yet.
 
+This line addresses incident 124 somehow.
+
 Here's some more perfectly fine code.
